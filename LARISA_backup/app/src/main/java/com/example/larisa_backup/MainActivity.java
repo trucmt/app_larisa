7930d34553c24ae767479855090bddf5783e9215
dearest <51027588+lightoverplay@users.noreply.github.com>
2024-01-17 23:05:21 +0700

    CME302: larisa_backup_front-end

---
+++ LARISA_backup/app/src/main/java/com/example/larisa_backup/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.larisa_backup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.larisa_backup.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.scanButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
